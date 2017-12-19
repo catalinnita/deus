6ff93c508a98ed5ff5b9bf51a72b8bc67adcd1e4
Catalin Nita <catalin.nita@immediate.co.uk>
2017-12-19 11:08:46 +0000

    test

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the footer - new text here some new text for test
+ * The template for displaying the footer - new text here some new text for test and some other new text
  *
  * Contains the closing of the #content div and all content after.
  *
